41bf6deb9bda8da3867526af752105a9b4189ff8
Prasanth-NJ1 <prasanthnj72@gmail.com>
2025-03-24 19:12:46 +0530

    Removed Unnecessary Files

--- db_config.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-def get_db():
-    CONNECTION_STRING ="mongodb+srv://prasanthnj72:Prasanth%4072@moviecluster.qd8li.mongodb.net/?retryWrites=true&w=majority&appName=MovieCluster"
-
-    client = MongoClient(CONNECTION_STRING)  # Change this if using a cloud database
-    return client["movie_recommendation"]
-
-    # prasanthnj72:Prasanth%4072
