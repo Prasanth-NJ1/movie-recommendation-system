66f49b92770f0760089389f609c36aa2f344a70d
Prasanth-NJ1 <prasanthnj72@gmail.com>
2025-03-26 18:12:45 +0530

    Deployment Changes

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from backend.app import app
+
+
 from backend.app import app
 
 if __name__ == "__main__":
